dd84ca3e2298450bb6694f2e7378c35cb1a55992
stivemok <stivemok@gmail.com>
2023-06-23 13:14:10 -0700

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # create a file in /tmp
 
-file {'school':
-  path    => '/tmp/school',
+file { '/tmp/school':
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love puppet'
-  }
+  content => 'I love puppet',
+}
