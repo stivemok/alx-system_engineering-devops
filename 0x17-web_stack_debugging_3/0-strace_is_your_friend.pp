78ea576618f829a32d7ba6f366b2664ffd8c3200
stivemok <stivemok@gmail.com>
2023-08-09 13:10:18 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Using strace, find out why Apache is returning a 500 error
 exec {'replace':
-     provider => shell,
-     command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
 }
