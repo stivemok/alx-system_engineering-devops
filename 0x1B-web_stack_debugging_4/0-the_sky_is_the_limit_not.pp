22d8e38e6cb5db1c758432a092fd16a784f3e1f5
stivemok <stivemok@gmail.com>
2023-08-17 13:48:09 -0700

    Sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 # Fix web stack debuggig 4
-exec{
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+  notify  => Exec['nginx-restart'],
 }
 
-exec {'restart':
-  provider => shell,
-  command  => 'nginx restart',
+exec { 'nginx-restart':
+  command     => 'nginx restart',
+  path        => '/etc/init.d/',
+  refreshonly => true,
 }
